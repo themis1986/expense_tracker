2f3a42d4d609b99e9b8c3234aa23aa7cb9553651
thiakovidis <thiakovidis@agroapps.gr>
2022-11-15 10:57:18 +0200

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  const { title, amount, date } = props;
+
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
